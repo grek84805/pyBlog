3d07a979f305806b0a8002cc1e66b84ef4535a12
Denys Madin <madinden84@gmail.com>
2022-10-20 10:40:33 +0200

    Start project

--- pyBlog/settings/base.py
+++ pyBlog/settings/base.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = "pyBlog.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
