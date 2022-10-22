ba3f503676d2b97a4d8d2b02b6cc61a7bba97df8
Denys Madin <madinden84@gmail.com>
2022-10-22 16:07:33 +0200

    add redis

--- pyBlog/settings/dev.py
+++ pyBlog/settings/dev.py
@@ -25,6 +25,18 @@ DATABASES = {
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+CACHE_TTL = 60 * 15
+
 try:
     from .local import *
 except ImportError:
